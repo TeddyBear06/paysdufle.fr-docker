993c23d1ead80fa175683d4f2c0cd662f20d5703
T3d <TeddyBear06@users.noreply.github.com>
2021-04-12 00:56:05 +0200

    Add search functionnality

---
+++ search/search.php
@@ -0,0 +1,63 @@
+<?php
+
+# On charge les librairies
+require __DIR__ . '/vendor/autoload.php';
+
+use Ehann\RedisRaw\PredisAdapter;
+use Ehann\RediSearch\Index;
+
+# Client redis
+$redis = (new PredisAdapter())->connect('redis', 6379);
+$contenuIndex = new Index($redis, 'contenuIndex');
+
+$recherche = $_GET['term'] . '*';
+
+$sousCategories = $contenuIndex
+    ->tagFilter('type', ['sousCategorie'])
+    ->search($recherche);
+
+$lecons = $contenuIndex
+    ->tagFilter('type', ['lecon'])
+    ->search($recherche);
+
+$resultats = [];
+$resultatsSousCategories = [];
+$resultatsLecons = [];
+
+if ($sousCategories->getCount()) {
+    $sousCategorieIndex = 1;
+    foreach ($sousCategories->getDocuments() as $sousCategorie) {
+        $resultatsSousCategories[] = [
+            'id' => $sousCategorieIndex,
+            'text' => $sousCategorie->nom . ' (' . $sousCategorie->categorie . ')',
+            'url' => $sousCategorie->url,
+        ];
+        $sousCategorieIndex++;
+    }
+}
+
+if ($lecons->getCount()) {
+    $leconIndex = 1;
+    foreach ($lecons->getDocuments() as $lecon) {
+        $resultatsLecons[] = [
+            'id' => $leconIndex,
+            'text' => $lecon->nom . ' (' . $lecon->categorie . ')',
+            'url' => $lecon->url,
+        ];
+        $leconIndex++;
+    }
+}
+
+$resultats = [
+    [
+        'text' => 'Thématiques',
+        'children' => $resultatsSousCategories,
+    ],
+    [
+        'text' => 'Leçons',
+        'children' => $resultatsLecons,
+    ],
+];
+
+header('Content-type: application/json');
+echo json_encode($resultats);
