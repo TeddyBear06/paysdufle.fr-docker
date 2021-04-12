4d13471ac9e0668e4a55d3e05aaad19dcba51e62
T3d <TeddyBear06@users.noreply.github.com>
2021-04-12 22:53:04 +0200

    Add lessons tags in search results

--- search/search.php
+++ search/search.php
@@ -10,7 +10,17 @@ use Ehann\RediSearch\Index;
 $redis = (new PredisAdapter())->connect('redis', 6379);
 $contenuIndex = new Index($redis, 'contenuIndex');
 
-$recherche = $_GET['term'] . '*';
+$recherche = $_GET['term'];
+
+$cleanedTags = str_replace(['\'', '"', ',' , ';', '<', '>'], '', $recherche);
+$cleanedTags = str_replace(['-'], ' ', $recherche);
+
+$tags = explode(' ', $cleanedTags);
+$queryTags = [];
+
+foreach ($tags as $tag) {
+    $queryTags[] = $tag . '*';
+}
 
 $sousCategories = $contenuIndex
     ->tagFilter('type', ['sousCategorie'])
@@ -20,16 +30,22 @@ $lecons = $contenuIndex
     ->tagFilter('type', ['lecon'])
     ->search($recherche);
 
+$contenusLecons = $contenuIndex
+    ->tagFilter('type', ['contenuLecon'])
+    ->tagFilter('tag', $queryTags)
+    ->search();
+
 $resultats = [];
 $resultatsSousCategories = [];
 $resultatsLecons = [];
+$resultatsContenusLecons = [];
 
 if ($sousCategories->getCount()) {
     $sousCategorieIndex = 1;
     foreach ($sousCategories->getDocuments() as $sousCategorie) {
         $resultatsSousCategories[] = [
             'id' => $sousCategorieIndex,
-            'text' => $sousCategorie->nom . ' (' . $sousCategorie->categorie . ')',
+            'text' => $sousCategorie->label . ' (' . $sousCategorie->categorie . ')',
             'url' => $sousCategorie->url,
         ];
         $sousCategorieIndex++;
@@ -41,8 +57,20 @@ if ($lecons->getCount()) {
     foreach ($lecons->getDocuments() as $lecon) {
         $resultatsLecons[] = [
             'id' => $leconIndex,
-            'text' => $lecon->nom . ' (' . $lecon->categorie . ')',
+            'text' => $lecon->label . ' (' . $lecon->categorie . ')',
             'url' => $lecon->url,
+        ];
+        $leconIndex++;
+    }
+}
+
+if ($contenusLecons->getCount()) {
+    $leconIndex = 1;
+    foreach ($contenusLecons->getDocuments() as $contenuLecon) {
+        $resultatsContenusLecons[] = [
+            'id' => $leconIndex,
+            'text' => $contenuLecon->label . ' (' . $contenuLecon->categorie . ')',
+            'url' => $contenuLecon->url,
         ];
         $leconIndex++;
     }
@@ -57,6 +85,10 @@ $resultats = [
         [
             'text' => 'Leçons',
             'children' => $resultatsLecons,
+        ],
+        [
+            'text' => 'Contenus de leçons',
+            'children' => $resultatsContenusLecons,
         ],
     ]
 ];
