232803c4776c0ad1d00dca26334a2268abb58c01
T3d <TeddyBear06@users.noreply.github.com>
2021-07-08 19:57:22 +0200

    Initial counter version

--- rgpdcvc/server.py
+++ rgpdcvc/server.py
@@ -1,10 +1,35 @@
-from flask import Flask
-import redis
+import websockets
+import asyncio
+import json
 
-app = Flask(__name__)
-r = redis.Redis(host='localhost', port=6379, db=0)
-p = r.pubsub()
+USERS = set()
 
-@app.route("/<random_id>")
-def register():
-    
+def users_event():
+    return json.dumps({"count": len(USERS)})
+
+async def notify_users():
+    if USERS:
+        message = users_event()
+        await asyncio.wait([user.send(message) for user in USERS])
+
+async def register(websocket):
+    USERS.add(websocket)
+    await notify_users()
+
+async def unregister(websocket):
+    USERS.remove(websocket)
+    await notify_users()
+
+async def counter(websocket, path):
+    await register(websocket)
+    try:
+        await websocket.send(state_event())
+        async for message in websocket:
+            # Nothing to do here
+    finally:
+        await unregister(websocket)
+
+start_server = websockets.serve(counter, '0.0.0.0', 8090)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
